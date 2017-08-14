10beb02ebccafc82e15f82c3f012a271fbbdce12
Alex Naidis <alex.naidis@linux.com>
2017-08-14 19:51:27 -0400

    PhoneWindowManager: Block screenshots when pocket lock is showing
    We should prevent accidental screenshots while pocket lock
    is showing.
    
    Change-Id: I9c88c2fd7b09cf0d3ee27871c08199a91dc310d9
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6284,7 +6284,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Assume this is called from the Handler thread.
     private void takeScreenshot(final int screenshotType) {
         synchronized (mScreenshotLock) {
-            if (mScreenshotConnection != null) {
+            if (mScreenshotConnection != null || mPocketLockShowing) {
                 return;
             }
             final ComponentName serviceComponent = new ComponentName(SYSUI_PACKAGE,
